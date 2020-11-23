beaf746e20a6dbfd5382416f7169905ca662b4eb
Manjot Singh <ManjotSingh@gmail.com>
2020-11-23 16:45:37 +1300

    Add project files.

---
+++ Russianroulletegun/GameForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Russianroulletegun
+{
+    partial class Russian_Roullete
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PicBox_Shoot = new System.Windows.Forms.PictureBox();
+            this.btn_Load = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Spin = new System.Windows.Forms.Button();
+            this.btn_Shoot = new System.Windows.Forms.Button();
+            this.btn_ShootAway = new System.Windows.Forms.Button();
+            this.btn_playAgain = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_Shoot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PicBox_Shoot
+            // 
+            this.PicBox_Shoot.Location = new System.Drawing.Point(243, 20);
+            this.PicBox_Shoot.Margin = new System.Windows.Forms.Padding(4);
+            this.PicBox_Shoot.Name = "PicBox_Shoot";
+            this.PicBox_Shoot.Size = new System.Drawing.Size(579, 416);
+            this.PicBox_Shoot.TabIndex = 1;
+            this.PicBox_Shoot.TabStop = false;
+            // 
+            // btn_Load
+            // 
+            this.btn_Load.BackColor = System.Drawing.Color.Olive;
+            this.btn_Load.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Load.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Load.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btn_Load.Location = new System.Drawing.Point(13, 70);
+            this.btn_Load.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Load.Name = "btn_Load";
+            this.btn_Load.Size = new System.Drawing.Size(160, 73);
+            this.btn_Load.TabIndex = 2;
+            this.btn_Load.Text = "Load";
+            this.btn_Load.UseVisualStyleBackColor = false;
+            this.btn_Load.Click += new System.EventHandler(this.btn_Load_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.BackColor = System.Drawing.Color.Olive;
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Exit.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btn_Exit.Location = new System.Drawing.Point(840, 276);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(193, 65);
+            this.btn_Exit.TabIndex = 3;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = false;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // btn_Spin
+            // 
+            this.btn_Spin.BackColor = System.Drawing.Color.Olive;
+            this.btn_Spin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Spin.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Spin.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btn_Spin.Location = new System.Drawing.Point(13, 188);
+            this.btn_Spin.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Spin.Name = "btn_Spin";
+            this.btn_Spin.Size = new System.Drawing.Size(160, 66);
+            this.btn_Spin.TabIndex = 4;
+            this.btn_Spin.Text = "Spin";
+            this.btn_Spin.UseVisualStyleBackColor = false;
+            this.btn_Spin.Click += new System.EventHandler(this.btn_Spin_Click);
+            // 
+            // btn_Shoot
+            // 
+            this.btn_Shoot.BackColor = System.Drawing.Color.Olive;
+            this.btn_Shoot.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Shoot.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Shoot.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btn_Shoot.Location = new System.Drawing.Point(860, 34);
+            this.btn_Shoot.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Shoot.Name = "btn_Shoot";
+            this.btn_Shoot.Size = new System.Drawing.Size(160, 73);
+            this.btn_Shoot.TabIndex = 5;
+            this.btn_Shoot.Text = "Shoot";
+            this.btn_Shoot.UseVisualStyleBackColor = false;
+            this.btn_Shoot.Click += new System.EventHandler(this.btn_shoot_Click);
+            // 
+            // btn_ShootAway
+            // 
+            this.btn_ShootAway.BackColor = System.Drawing.Color.Olive;
+            this.btn_ShootAway.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_ShootAway.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ShootAway.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btn_ShootAway.Location = new System.Drawing.Point(840, 155);
+            this.btn_ShootAway.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_ShootAway.Name = "btn_ShootAway";
+            this.btn_ShootAway.Size = new System.Drawing.Size(214, 66);
+            this.btn_ShootAway.TabIndex = 6;
+            this.btn_ShootAway.Text = "Shoot Away";
+            this.btn_ShootAway.UseVisualStyleBackColor = false;
+            this.btn_ShootAway.Click += new System.EventHandler(this.btn_ShootAway_Click);
+            // 
+            // btn_playAgain
+            // 
+            this.btn_playAgain.BackColor = System.Drawing.Color.Olive;
+            this.btn_playAgain.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_playAgain.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_playAgain.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btn_playAgain.Location = new System.Drawing.Point(13, 318);
+            this.btn_playAgain.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_playAgain.Name = "btn_playAgain";
+            this.btn_playAgain.Size = new System.Drawing.Size(193, 65);
+            this.btn_playAgain.TabIndex = 7;
+            this.btn_playAgain.Text = "Play Again";
+            this.btn_playAgain.UseVisualStyleBackColor = false;
+            this.btn_playAgain.Click += new System.EventHandler(this.btn_playAgain_Click);
+            // 
+            // Russian_Roullete
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1067, 449);
+            this.Controls.Add(this.btn_playAgain);
+            this.Controls.Add(this.btn_ShootAway);
+            this.Controls.Add(this.btn_Shoot);
+            this.Controls.Add(this.btn_Spin);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Load);
+            this.Controls.Add(this.PicBox_Shoot);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Russian_Roullete";
+            this.Text = "Russian_Roullete";
+            this.Load += new System.EventHandler(this.Russian_Roullete_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox_Shoot)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox PicBox_Shoot;
+        private System.Windows.Forms.Button btn_Load;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Spin;
+        private System.Windows.Forms.Button btn_Shoot;
+        private System.Windows.Forms.Button btn_ShootAway;
+        private System.Windows.Forms.Button btn_playAgain;
+    }
+}
---
+++ Russianroulletegun/Roullete.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Russianroulletegun
+{
+    public class Roullete
+    {
+        public int load; // Variable to hold the position of the bullet at the time of loading bullet in chamber
+        public int spin_Bullet; // variable to hold position of the bullet after spinning the chamber
+        public int Total_Fire = 6;//sets the total number of fire
+        public int Point_Away = 2;//set the number of chances to shoot away bullet
+        Random Rand = new Random();
+        public void Load()
+        {
+            load = 1;
+        }
+
+        public void spin()
+        {
+            spin_Bullet = Rand.Next(1, 7); // assign random position between 1 to 6
+        }
+
+        public int Bulleteloop(int skip)
+        {
+            if (skip == 6)
+            {
+                skip = 1;
+            }
+            else
+            {
+                skip++;
+            }
+            return skip;
+        }
+    }
+}
